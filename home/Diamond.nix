a2fd048c242bcd0e0d35e7f2b327445ad08329c0
VadimBayramov <89941579+VadimBayramov@users.noreply.github.com>
2025-07-30 21:07:08 +0100

    Update Diamond.nix

--- home/Diamond.nix
+++ home/Diamond.nix
@@ -1,10 +1,12 @@
 { config, pkgs, ... }:
 
 {
+  # Shell, терминал и редактор
   programs.fish.enable = true;
   programs.kitty.enable = true;
   programs.vim.enable = true;
 
+  # Пакеты пользователя
   home.packages = with pkgs; [
     spotify
     firefox
@@ -13,22 +15,31 @@
     remmina
     electrum
     wireguard-tools
-    pea
+    peazip          # поправил название (pea → peazip)
     vlc
     discord
     wine
     git
   ];
 
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
+  # Системные сервисы не включаются тут, но можно юзерские, если нужны (pipewire клиент)
+  # Обычно PipeWire daemon включается на уровне системы
+  # Здесь можно настроить клиентские утилиты, например:
+  environment.variables = {
+    PIPEWIRE_ENABLE = "1";  # если нужно
   };
 
-  # Flatpak (для Spotify и Discord, если нужны свежие версии)
+  # Flatpak для свежих версий некоторых программ
   programs.flatpak.enable = true;
   home.file.".config/flatpak/overrides".text = ''
     [Context]
     shared=ipc,network,xdg-runtime
   '';
+
+  # Конфигурация Hyprland (опционально)
+  # Если хочешь, здесь можно добавить пользовательские конфиги и автозапуск:
+  home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;  # твой локальный конфиг
+
+  # Автоматический запуск программ в Hyprland через home manager
+  # Можно добавить user units systemd для автозапуска (например, pipewire, wireplumber, polkit, dunst)
 }
