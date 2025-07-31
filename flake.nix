62e8fc3785caa1883e44b298e4437f38de1c5595
VadimBayramov <89941579+VadimBayramov@users.noreply.github.com>
2025-07-31 17:46:21 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,14 +2,10 @@
   description = "NixOS config for Pavilion with Hyprland, PipeWire, Wine and needed apps";
 
   inputs = {
-    # Основной набор пакетов NixOS 24.05 стабильный
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-
-    # Home Manager для управления настройками пользователя
     home-manager.url = "github:nix-community/home-manager/release-24.05";
-
-    # Репозиторий с конфигами и dotfiles Hyprland от end-4
-    illogical-impulse.url = "github:end-4/dots-hyprland-nixos";
+    illogical-impulse.url = "github:bigsaltyfishes/end-4-dots";
+    illogical-impulse.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, illogical-impulse, ... }:
@@ -19,11 +15,10 @@
     in {
       nixosConfigurations.Pavilion = nixpkgs.lib.nixosSystem {
         inherit system;
-
         modules = [
-          ./hosts/pavilion.nix                            # Основной системный конфиг для Pavilion
-          home-manager.nixosModules.home-manager          # Включаем home-manager как модуль NixOS
-          illogical-impulse.homeManagerModules.default    # Модуль dotfiles и настроек Hyprland от end-4
+          ./hosts/pavilion.nix
+          home-manager.nixosModules.home-manager
+          illogical-impulse.homeManagerModules.default
         ];
       };
     };
