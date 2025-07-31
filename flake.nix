a144e1086831e556ce14b974dad3f625def1fe34
VadimBayramov <89941579+VadimBayramov@users.noreply.github.com>
2025-07-31 23:27:55 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,36 +1,41 @@
-# flake.nix
 {
   description = "Моя NixOS + Illogical Impulse";
 
   inputs = {
-    # Меняем канал на 24.05
+    # Стабильный канал 24.05
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-24.05";
     };
     home-manager = {
       url = "github:nix-community/home-manager";
+      # привязываем к тому же nixpkgs
       inputs.nixpkgs.follows = "nixpkgs";
     };
     illogical-impulse = {
       url = "github:bigsaltyfishes/end-4-dots";
+      # тоже используем тот же nixpkgs
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, illogical-impulse, ... }:
-    let system = "x86_64-linux"; in {
-      nixosConfigurations.system = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./disko.nix
-          ./home/configuration.nix
-          home-manager.nixosModules.home-manager
-          illogical-impulse.homeManagerModules.default
-        ];
-      };
-      homeConfigurations.user = home-manager.lib.homeManagerConfiguration {
-        inherit system;
-        modules = [ ./home-manager/home.nix ];
-      };
+  outputs = { self, nixpkgs, home-manager, illogical-impulse, ... }: let
+    system = "x86_64-linux";
+  in {
+    # Сборка системы NixOS
+    nixosConfigurations.system = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./disko.nix
+        ./home/configuration.nix
+        home-manager.nixosModules.home-manager
+        illogical-impulse.homeManagerModules.default
+      ];
     };
+
+    # (Опционально) конфигурация Home-Manager для пользователя
+    homeConfigurations.user = home-manager.lib.homeManagerConfiguration {
+      inherit system;
+      modules = [ ./home-manager/home.nix ];
+    };
+  };
 }
