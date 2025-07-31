3eda2173f490a08bd6cdabd2d82f5dc1be9f9386
VadimBayramov <89941579+VadimBayramov@users.noreply.github.com>
2025-07-31 19:51:32 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,25 +1,35 @@
+# flake.nix
 {
-  description = "NixOS config for Pavilion with Hyprland, PipeWire, Wine and needed apps";
+  description = "Моя NixOS + Illogical Impulse (end-4-dots)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
-    illogical-impulse.url = "github:bigsaltyfishes/end-4-dots";
-    illogical-impulse.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs           = { url = "nixos-24.05"; };
+    home-manager      = { url = "github:nix-community/home-manager"; };
+    illogical-impulse = {
+      url = "github:bigsaltyfishes/end-4-dots";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, illogical-impulse, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
     in {
-      nixosConfigurations.Pavilion = nixpkgs.lib.nixosSystem {
+      # System-конфигурация (единственный «host»)
+      nixosConfigurations.system = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
-          ./hosts/pavilion.nix
+          ./disko.nix                   # если нужна автоматическая разметка
+          ./nixos/configuration.nix
           home-manager.nixosModules.home-manager
           illogical-impulse.homeManagerModules.default
         ];
+      };
+
+      # Домашняя конфигурация (необязательно)
+      homeConfigurations.user = home-manager.lib.homeManagerConfiguration {
+        inherit system;
+        modules = [ ./home-manager/home.nix ];
       };
     };
 }
