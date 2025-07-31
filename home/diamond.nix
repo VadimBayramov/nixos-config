1df6f3dee1e68a4a5ba8be785e101ae1d0554cd2
VadimBayramov <89941579+VadimBayramov@users.noreply.github.com>
2025-07-31 17:14:53 +0100

    Update diamond.nix

--- home/diamond.nix
+++ home/diamond.nix
@@ -1,64 +1,22 @@
-{ config, pkgs, ... }:
+{ inputs, ... }:
 
 {
-  # === Shell и терминалы ===
-  programs.fish.enable = true;       # Fish shell
-  programs.kitty.enable = true;      # Kitty терминал
-  programs.vim.enable = true;        # Vim редактор
-
-  # === Основные приложения для пользователя ===
-  home.packages = with pkgs; [
-    spotify
-    telegram-desktop
-    firefox
-    tor-browser
-    libreoffice
-    remmina
-    electrum
-    wireguard-tools
-    pea
-    vlc
-    discord
-    wine
-    git
-
-    # === Файловый менеджер Thunar с автоподключением внешних носителей ===
-    thunar                 # Сам файловый менеджер
-    thunar-volman          # Поддержка автоподключения и монтирования
-    gvfs                   # Необходим для работы Thunar с внешними накопителями
-    udisks                 # Демон управления дисками
+  imports = [
+    ./hardware-configuration.nix          # конфигурация железа (будешь дописывать)
+    ./packages/00-bundle_of_packages.nix  # твои пакеты и приложения
+    ./modules/00-bundle_of_modules.nix    # остальные модули (звук, hyprland, dotfiles и др.)
   ];
 
-  # === PipeWire (через home-manager, дублирует системный, но конфликтов обычно нет) ===
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
+  networking.hostName = "sunless";           # hostname, можно заменить на нужный
+
+  time.timeZone = "Europe/Moscow";         # часовой пояс
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = [ "en_US.UTF-8" "ru_RU.UTF-8" ];
   };
 
-  # === Flatpak для некоторых приложений, чтобы иметь свежие версии ===
-  programs.flatpak.enable = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ]; # включаем experimental flakes и команды
 
-  home.file.".config/flatpak/overrides".text = ''
-    [Context]
-    shared=ipc,network,xdg-runtime
-  '';
-
-  # === Конфигурация end-4 dots-hyprland ===
-  illogical-impulse = {
-    enable = true;
-
-    hyprland = {
-      package = pkgs.hyprland;                             # Используем пакет Hyprland из nixpkgs
-      xdgPortalPackage = pkgs.xdg-desktop-portal-hyprland; # Для правильной работы приложений на Wayland
-
-      ozoneWayland.enable = true;                          # Включаем поддержку ozone-wayland (рекомендуется для Hyprland)
-    };
-
-    dotfiles = {
-      anyrun.enable = true;   # dotfiles из набора end-4
-      fish.enable = true;
-      kitty.enable = true;
-      # Можно добавить другие по необходимости
-    };
-  };
+  system.stateVersion = "23.05";            # версия состояния системы (важно для обновлений)
 }
