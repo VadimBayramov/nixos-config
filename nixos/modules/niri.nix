6a6ae49a8897a7d97a5217cbf5916de6f0ae5207
Sunless <89941579+VadimBayramov@users.noreply.github.com>
2026-03-26 15:17:29 +0300

    Enable seatd, polkit, and greetd services

--- nixos/modules/niri.nix
+++ nixos/modules/niri.nix
@@ -1,4 +1,16 @@
 { ... }:
 {
   programs.niri.enable = true;
+  services.seatd.enable = true;
+  security.polkit.enable = true;
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "niri-session";
+        user = "sunless";
+      };
+    };
+  };
 }
